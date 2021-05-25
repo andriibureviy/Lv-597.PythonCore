58fa9599d48b778583a4510d5869c02a36d85529
andriibureviy <cococochka2@gmail.com>
2021-05-25 12:47:02 +0300

    Add HW4(4)

--- HW4/AndriiYazvinskyi/fib.py
+++ HW4/AndriiYazvinskyi/fib.py
@@ -1,9 +1,9 @@
 num1 = int(input("Enter ur num for Fibonacci: "))
 while num1 <= 0:  # check if the number is valid
-   print("Please enter a positive integer")
-   num1 = int(input("Enter ur num for Fibonacci: "))
+    print("Please enter a positive integer")
+    num1 = int(input("Enter ur num for Fibonacci: "))
 
-num1 += 1
+
 list1 = [0, 1]
 i = 0
 
@@ -12,12 +12,18 @@ while i < num1:
     list1.append(num2)
     i += 1
 
-#list1 = list1[:len(list1)-1] #remove last n elements from list
+#while list1[-1] > num1:
+#    list1 = list1[:(list1) - 1] #remove last n elements from list
 
 while list1[-1] > num1:
-    list1 = list1[:len(list1) - 1]
-else:
-    list1 = list1[:len(list1) + 1]
+   del list1[-1] #remove last n elements from list
 
-for b in range(num1):
-    print(list1[b], end= ",")
+#while list1[-1] > num1:
+#    list1 = list1[:-1] #remove last n elements from list
+
+#for i in range(0, len(list1)):
+#       if list1[i] > num1: #trying to remove elements from list
+#           list1.pop(i)
+
+else:
+    print(list1, end=", ")
